fba6db99fea4019b1d7cdb91add52224b70661aa
Seizure <78146652+ConvulsiveSeizure@users.noreply.github.com>
2021-04-25 18:33:51 +0300

    v.02

--- main.js
+++ main.js
@@ -6,13 +6,34 @@ $(document).ready(() => {
     $(window).scroll(function() {
         var scrolled = $(window).scrollTop();
     
-        if ( scrolled > 80 && scrolled > scrollPrev ) {
-            header.addClass('out');
-            $("#catalogMenu").addClass("outing")
+        if ( scrolled > 350 && scrolled > scrollPrev ) {
+            header.addClass('position-fixed');
         } else {
-            header.removeClass('out');
-            $("#catalogMenu").removeClass("outing")
+            header.removeClass('position-fixed');
         }
         scrollPrev = scrolled;
+
+        if (scrolled > 300) {
+            $("#catalogToFixed").addClass("toFixedTop")
+        } else {
+            $("#catalogToFixed").removeClass("toFixedTop")
+        }
     });
+
+
+
+    //catalog filter
+    let catArray = []
+    $(".catalogMenuBtn").on("click", (e) => {
+        let isAlreadyExist = false
+        catArray.map((i, index) => {
+            if (i == e.target.dataset.typeId) {
+                catArray.splice(index, 1)
+                isAlreadyExist = true
+            }
+        })
+        if (!isAlreadyExist) {
+            catArray.unshift(e.target.dataset.typeId)
+        }
+    })
 })
